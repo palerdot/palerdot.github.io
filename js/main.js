a899b11d6527189de270207de23797ef416e773f
Arun <palerdot@gmail.com>
2016-12-08 22:25:04 +0530

    [initial commit] revample website profile

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var APP = {
+
+    init: function () {
+
+    }
+
+};
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
+
+$(document).ready( function () {
+    console.log("doc ready");
+} );
